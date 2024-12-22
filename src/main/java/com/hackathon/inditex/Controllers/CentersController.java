cbf37ae76b181ae4105224053967fdfe197c0ae2
jgarcia6 <jgarcia6@werfen.com>
2024-12-22 20:28:00 +0100

    Try to improve

--- src/main/java/com/hackathon/inditex/Controllers/CentersController.java
+++ src/main/java/com/hackathon/inditex/Controllers/CentersController.java
@@ -1,12 +1,15 @@
 package com.hackathon.inditex.Controllers;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 import com.hackathon.inditex.Entities.Center;
 import com.hackathon.inditex.Services.CentersService;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -27,33 +30,26 @@ public class CentersController {
     this.centersService = centersService;
   }
 
-  @PostMapping
-  public ResponseEntity<MessageResponse> createNewLogisticsCenter(@RequestBody @Validated Center center) {
+  @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+  public ResponseEntity<Map<String, String>> createNewLogisticsCenter(@RequestBody Center center) {
     centersService.createNewLogisticsCenter(center);
-    return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(new MessageResponse("Logistics center created successfully."));
+    return ResponseEntity.status(HttpStatus.CREATED).body(Collections.singletonMap("message", "Logistics center created successfully."));
   }
 
-  @GetMapping
+  @GetMapping(produces = APPLICATION_JSON_VALUE)
   public ResponseEntity<Collection<Center>> retrieveAllLogisticsCenters() {
     return ResponseEntity.ok(centersService.retrieveAllLogisticsCenters());
   }
 
-  @PatchMapping("/{id}")
-  public ResponseEntity<MessageResponse> updateDetailsOfAnExistingLogisticsCenter(@PathVariable Long id,
-      @RequestBody @Validated Center center) {
+  @PatchMapping(path = "/{id}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+  public ResponseEntity<Map<String, String>> updateDetailsOfAnExistingLogisticsCenter(@PathVariable Long id, @RequestBody Center center) {
     centersService.updateDetailsOfAnExistingLogisticsCenter(id, center);
-    return ResponseEntity.ok(new MessageResponse("Logistics center updated successfully."));
+    return ResponseEntity.ok(Collections.singletonMap("message", "Logistics center updated successfully."));
   }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<?> deleteALogisticsCenter(@PathVariable Long id) {
+  @DeleteMapping(path = "/{id}", produces = APPLICATION_JSON_VALUE)
+  public ResponseEntity<Map<String, String>> deleteALogisticsCenter(@PathVariable Long id) {
     centersService.deleteALogisticsCenter(id);
-    return ResponseEntity.ok(new MessageResponse("Logistics center deleted successfully."));
-  }
-
-  public record MessageResponse(String message) {
-
+    return ResponseEntity.ok(Collections.singletonMap("message", "Logistics center deleted successfully."));
   }
 }
