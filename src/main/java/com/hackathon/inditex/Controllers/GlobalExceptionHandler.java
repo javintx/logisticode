bf7e445ff16bf3ad197571959ece9ef2fbe75fdf
jgarcia6 <jgarcia6@werfen.com>
2025-01-09 13:39:17 +0100

    Improve code

---
+++ src/main/java/com/hackathon/inditex/Controllers/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.hackathon.inditex.Controllers;
+
+import com.hackathon.inditex.DTO.MessageResponse;
+import com.hackathon.inditex.Services.CentersService.CenterNotFoundException;
+import com.hackathon.inditex.Services.CentersService.LoadCapacityExceededException;
+import com.hackathon.inditex.Services.CentersService.LocationAlreadyInUseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(LocationAlreadyInUseException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public MessageResponse handleLogisticCenterLocationException(LocationAlreadyInUseException ex) {
+    return new MessageResponse(ex.getMessage());
+  }
+
+  @ExceptionHandler(LoadCapacityExceededException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public MessageResponse handleLogisticCenterLocationException(LoadCapacityExceededException ex) {
+    return new MessageResponse(ex.getMessage());
+  }
+
+  @ExceptionHandler(CenterNotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public MessageResponse handleCenterNotFoundException(CenterNotFoundException ex) {
+    return new MessageResponse(ex.getMessage());
+  }
+}
