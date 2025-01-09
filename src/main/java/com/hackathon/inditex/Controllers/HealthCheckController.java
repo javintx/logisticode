0ec3b8b72cd9a97d99f051d0b80c9fd879654d33
jgarcia6 <jgarcia6@werfen.com>
2025-01-09 12:57:12 +0100

    Revert "Improve code"
    This reverts commit cf4921bca15d8f575f15bd3eafe9a79da7eff77b.

--- src/main/java/com/hackathon/inditex/Controllers/HealthCheckController.java
+++ src/main/java/com/hackathon/inditex/Controllers/HealthCheckController.java
@@ -1,15 +1,27 @@
 package com.hackathon.inditex.Controllers;
 
+import com.hackathon.inditex.Services.HealthCheckService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
 public class HealthCheckController {
 
+  private final HealthCheckService healthCheckService;
+
+  @Autowired
+  public HealthCheckController(HealthCheckService healthCheckService) {
+    this.healthCheckService = healthCheckService;
+  }
+
   @GetMapping
+  @ResponseStatus(HttpStatus.OK)
   public String healthCheck() {
-    return "API is working";
+    return healthCheckService.checkHealth();
   }
 }
