e43e3d3710375a6adf05026981b3dd8374c5271b
jgarcia6 <jgarcia6@werfen.com>
2025-01-09 10:35:45 +0100

    Improve code

--- src/main/java/com/hackathon/inditex/Controllers/HealthCheckController.java
+++ src/main/java/com/hackathon/inditex/Controllers/HealthCheckController.java
@@ -1,5 +1,7 @@
 package com.hackathon.inditex.Controllers;
 
+import com.hackathon.inditex.Services.HealthCheckService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthCheckController {
 
+  private final HealthCheckService healthCheckService;
+
+  @Autowired
+  public HealthCheckController(HealthCheckService healthCheckService) {
+    this.healthCheckService = healthCheckService;
+  }
+
   @GetMapping
   public String healthCheck() {
-    return "API is working";
+    return healthCheckService.checkHealth();
   }
 }
