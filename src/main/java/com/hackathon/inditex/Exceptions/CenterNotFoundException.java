19b4c20470d8eaca0b1980ca22bab8637b64bf16
jgarcia6 <jgarcia6@werfen.com>
2025-01-09 12:25:03 +0100

    Improve code

--- src/main/java/com/hackathon/inditex/Exceptions/CenterNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.hackathon.inditex.Exceptions;
-
-public class CenterNotFoundException extends RuntimeException {
-
-  public CenterNotFoundException() {
-    super("Center not found.");
-  }
-}
